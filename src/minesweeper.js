7aa74e4af74e593d1adb0fef0dd5ca3d494e45a0
Jess <jjohnson4927@gmail.com>
2017-10-19 20:18:00 -0500

    Add bombLine and guessLine

---
+++ src/minesweeper.js
@@ -0,0 +1,7 @@
+var blankLine = '  |   |  ';
+console.log("This is what an empty board would look like:");
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+const guessLine = ' 1 |   |  ';
