fea69c83eccf3181f409bae783dea4d0cfaddda9
Jess <jjohnson4927@gmail.com>
2017-10-23 06:13:38 -0500

    Print array-based game board, transpile

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,15 +1,19 @@
 const printBoard = (board) => {
   console.log("Current Board:");
 
-  console.log(board[0].join(" | "));
-  console.log(board[1].join(" | "));
-  console.log(board[2].join(" | "));
+  console.log(board[0].join(" | "))
+  console.log(board[1].join(" | "))
+  console.log(board[2].join(" | "))
 }
 
 const board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
   [' ', ' ', ' ']
-];
+]
 
-console.log(printBoard(board));
+console.log(printBoard(board))
+
+board[0][1] = '1';
+board[2][2] = 'B';
+console.log(printBoard(board))
